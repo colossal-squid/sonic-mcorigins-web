39f1130b9c227c421ba8c7cb1ac62d4d6407869a
Roman Guivan <guivanrv@gmail.com>
2023-10-24 11:03:19 +0200

    copy files in build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     app: Path.resolve(__dirname, '../src/scripts/index.js'),
   },
   output: {
-    path: Path.join(__dirname, '../build'),
+    path: Path.join(__dirname, '../docs'),
     filename: 'js/[name].js',
   },
   optimization: {
@@ -20,7 +20,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
-      patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
+      patterns: [
+        { from: Path.resolve(__dirname, '../public'), to: '' }],
     }),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html'),
